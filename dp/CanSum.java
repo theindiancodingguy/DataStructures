94a59d25f21fe506cf9137fd9e9534c3d9deb1a5
Shriram Nirmal <shrirammathesh24@gmail.com>
2021-05-02 21:26:11 +0530

    completed a memoization apprach of the can sum problem

--- dp/CanSum.java
+++ dp/CanSum.java
@@ -11,6 +11,7 @@ import java.util.*;
 
 class CanSum{
 
+// Recursie Approach
 boolean canSum_rec(int sum, int a[]){
 if(sum == 0) return true;
 if(sum < 0 ) return false;
@@ -20,9 +21,28 @@ if(sum < 0 ) return false;
 	return true;
 	}
 return false; // try all possiblity in for loop else return false
+//  Time complexity O(n ^ m)
+// Space Complexity O(m)
+}
+
+//memoization approach
+Boolean canSum_mem(int sum,int a[],HashMap hm){
+if(sum == 0){Boolean True = new Boolean(true); return True;}
+if(sum <0){Boolean False = new Boolean(false);return False;}
+if(hm.containsKey(sum)) return (Boolean)hm.get(sum);
+for(int i=0;i<a.length;i++){
+	int rem = sum - a[i];
+        hm.put(sum,canSum_mem(rem,a,hm));
+	if((Boolean)hm.get(sum) ==  true)   
+	{Boolean True = new Boolean(true); return True;}
+	}
+Boolean False = new Boolean(false);return False;
+// Time complexity O(n + m)
+//Space complexity O(m) 
 }
 
 public static void main(String args[]){
+HashMap<Integer,Boolean> hm  = new HashMap<>();
 Scanner in = new Scanner(System.in);
 System.out.print("Enter the array size : ");
 int n = in.nextInt();
@@ -32,6 +52,7 @@ int a[] =  new int [n];
 	a[i] = in.nextInt();
 System.out.print("Enter the sum:");
 int sum = in.nextInt();
+System.out.println("Getting the required sum with the array elements is " + new CanSum().canSum_mem(sum,a,hm));
 System.out.println("Getting the required sum with the array elements is " + new CanSum().canSum_rec(sum,a));
 }
 }
