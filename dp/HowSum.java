f7ed2ac89d2251867853c5dd23b035e84a9b9913
Shriram Nirmal <shreeram.cse@gmail.com>
2021-05-02 22:20:27 +0530

    Added a new dp problem statement

---
+++ dp/HowSum.java
@@ -0,0 +1,24 @@
+/* Write a function howSum(targetSum,numbers) that takes ina targetsum
+and an array of numbers as arguments
+
+The function should return an array containing any combination of 
+elements that add up to exactly the targetSum,if there is no 
+combination that adds up to the targget sum then return null
+
+
+If there are multiple combinations possible , you may return any 
+single one*/
+
+import java.util.*;
+
+class HowSum{
+
+
+
+
+public static void main(String args[]){
+Scanner in = new Scanner(System.in);
+ArrayList<Integer> num = new  ArrayList<>();
+System.out.print();
+}
+}
