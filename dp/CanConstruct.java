6de139f8690d0df8cd33d2e5f381bdde328149d8
Shriram Nirmal <shreeram.cse@gmail.com>
2021-05-05 22:53:10 +0530

    Added comment lines explaining time complexity for can construct code

--- dp/CanConstruct.java
+++ dp/CanConstruct.java
@@ -12,6 +12,7 @@ import java.util.*;
 
 class CanConstruct{
 // recursie approach
+
 boolean canConstruct_rec(String word,String [] wordBank){
 if(word.length() == 0) return true;
 for(int i = 0;i<wordBank.length;i++){
@@ -21,13 +22,39 @@ if(word.indexOf(wordBank[i]) == 0){
  }
 }
 return false;
+/*
+length of the word - m
+length of the wordBank - n
+Time complexity - O(n ^ m * m)
+Space Complexity - O(m)
+*/
+}
+
+//memoization approach
+Boolean canConstruct_mem(String word,String [] wordBank,HashMap hm){
+if(hm.containsKey(word)) return (Boolean)hm.get(word); 
+for(int i=0;i<wordBank.length;i++){
+if(word.indexOf(wordBank[i]) == 0){
+  int suffix = wordBank[i].length();
+  hm.put(word,canConstruct_mem(word.substring(suffix),wordBank,hm));
+  if((Boolean)hm.get(word)){ Boolean True = new Boolean(true); return True;}
+  }
+ }
+Boolean False = new Boolean(false);
+return False;
+/*
+Time complexity - O (n * m^2)
+Space complexity - O (m^2)
+*/
 }
 
 public static void main(String args[]){ 
 Scanner in = new Scanner(System.in);
+HashMap<String,Boolean> hm = new HashMap<>();
 //String [] wordBank = {"ab","abc","cd","def","abcd"};
 //System.out.println("Construction is: "+new CanConstruct().canConstruct_rec("abcdef",wordBank));
-String [] wordBank = {"bo","rd","ate","t","ska","sk","boar"};
-System.out.println("Construction is: "+new CanConstruct().canConstruct_rec("skateboard",wordBank));
+String [] wordBank = {"e","ee","eee","eeee","eeeee","eeeeee","eeeeeee"};
+System.out.println("Construction is: "+new CanConstruct().canConstruct_mem("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",wordBank,hm));
+System.out.println("Construction is: "+new CanConstruct().canConstruct_rec("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",wordBank));
 }
 }
